3faefa1552d92ea9a678947c54020f5310457681
sputnik555 <79382246+sputnik555@users.noreply.github.com>
2022-05-23 20:21:52 +0300

    Customize Flat Admin.

--- property/admin.py
+++ property/admin.py
@@ -5,6 +5,7 @@ from .models import Flat
 
 class Admin(admin.ModelAdmin):
     search_fields = ['town', 'owner', 'address']
+    readonly_fields = ['created_at']
 
 
 admin.site.register(Flat, Admin)
