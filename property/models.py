e8e4712345e93b0dc9b8f6341ae8ebbb288e33c5
sputnik555 <79382246+sputnik555@users.noreply.github.com>
2022-05-26 06:55:19 +0300

    delete excess fields

--- property/models.py
+++ property/models.py
@@ -6,9 +6,6 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
-    owner = models.CharField('ФИО владельца', max_length=200)
-    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
-    owner_pure_phone = PhoneNumberField('Нормализованный номер телефона', null=True, blank=True)
     created_at = models.DateTimeField(
         'Когда создано объявление',
         default=timezone.now,
@@ -80,7 +77,6 @@ class Complaint(models.Model):
 
 class Owner(models.Model):
     name = models.CharField('ФИО владельца', max_length=200)
-    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
     owner_pure_phone = PhoneNumberField('Нормализованный номер телефона', null=True, blank=True)
     flats = models.ManyToManyField(
         Flat,
